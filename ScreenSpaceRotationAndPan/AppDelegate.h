8344cf3641780805330f554094096ea6e4cc00b3
Johan Hiemstra <johanhiems@gmail.com>
2018-01-31 16:23:16 +0100

    Initial Commit

---
+++ ScreenSpaceRotationAndPan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScreenSpaceRotationAndPan
+//
+//  Created by Admin on 1/31/18.
+//  Copyright © 2018 Xartec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScreenSpaceRotationAndPan/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  ScreenSpaceRotationAndPan
+//
+//  Created by Admin on 1/31/18.
+//  Copyright © 2018 Xartec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
